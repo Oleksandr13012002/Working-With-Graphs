76a9203043065915372a95559cf268a7dc3dd799
Oleksandr13012002 <guzoleksandr37@gmail.com>
2025-05-09 10:53:15 +0300

    initial commit

---
+++ src/app/Graph.java
@@ -0,0 +1,28 @@
+package app;
+
+public class Graph {
+
+    public void addVertex(int vertex){
+
+    }
+
+    public void addEdge(int source, int destination){
+
+    }
+
+    public void removeVertex(int vertex){
+
+    }
+
+    public void removeEdge(int source, int destination){
+
+    }
+
+    public boolean hasVertex(int vertex){
+
+    }
+
+    public boolean hasEdge(int source, int destination){
+
+    }
+}
